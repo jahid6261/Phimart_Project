1cffd51e3158fef758b3c61ffb1a255d6a1e1dde
Jahid Alam <jahidalam01741@gmail.com>
2025-09-20 22:17:14 +0600

    initial commit

---
+++ product/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator,MaxLengthValidator
+from product.validators import validate_file_size
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField() 
+
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-id',]
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(
+        upload_to="products/images/", validators=[validate_file_size])
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1),MaxLengthValidator(5)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Review of{self.user.first.name} on {self.product.name}'
+# step to build an API
+# Model
+# Serializer
+# Viewset
+# router
+
+"""
+<Product: Laptop> id, name
+product_id
+product_name
+"""
